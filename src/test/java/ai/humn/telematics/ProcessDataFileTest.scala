33e24539b628b3882b1e54e4a6ad8c096448b210
GauthamPrakash14 <36693382+GauthamPrakash14@users.noreply.github.com>
2024-06-14 00:58:43 +0100

    Some more tests

--- src/test/java/ai/humn/telematics/ProcessDataFileTest.scala
+++ src/test/java/ai/humn/telematics/ProcessDataFileTest.scala
@@ -3,6 +3,7 @@ package ai.humn.telematics
 import java.io.{ByteArrayOutputStream, PrintStream}
 import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+//import org.scalatest.funspec.AnyFunSpec
 import org.scalatestplus.junit.JUnitRunner
 import ai.humn.telematics.ProcessDataFile.distance
 
@@ -11,6 +12,7 @@ class ProcessDataFileTest extends FlatSpec with Matchers {
 
   val testFilePath = this.getClass.getClassLoader.getResource("2021-10-05_journeys.csv")
 
+  behavior of "TestSuit"
   // This test is failing but I don't know why :(
   it should "generate results to stdout" in {
     //println(testFilePath.getPath())
@@ -32,6 +34,40 @@ class ProcessDataFileTest extends FlatSpec with Matchers {
   it should "Read a CSV file when path of file is provided" in {
     var filePath = "/Users/gautham/Interview assessment/AON/de-coding-test-gautham-prakash/src/test/resources/2021-10-05_journeys.csv"
     val j = ProcessDataFile.getData(filePath)
-    assert(j!=null)
+    assert(j != null)
+  }
+
+  behavior of "Solution to problems"
+  var filePath = "/Users/gautham/Interview assessment/AON/de-coding-test-gautham-prakash/src/test/resources/2021-10-05_journeys.csv"
+  val j = ProcessDataFile.getData(filePath)
+  it should "Find journeys that are more than 90 minutes" in {
+    //ProcessDataFile.journey90(j)
+    val baos = new ByteArrayOutputStream()
+    val ps = new PrintStream(baos)
+    Console.withOut(ps) {
+      ProcessDataFile.journey90(j)
+    }
+    val output = baos.toString
+    assert(output !=null)
+  }
+
+  it should "Find the average speed per journey in kph" in {
+    val baos = new ByteArrayOutputStream()
+    val ps = new PrintStream(baos)
+    Console.withOut(ps) {
+      ProcessDataFile.avgSpeedPerJourney(j)
+    }
+    val output = baos.toString
+    assert(output !=null)
+  }
+
+  it should "Find the mileage per driver and most active driver" in {
+    val baos = new ByteArrayOutputStream()
+    val ps = new PrintStream(baos)
+    Console.withOut(ps) {
+      ProcessDataFile.mileageByDriver(j)
+    }
+    val output = baos.toString
+    assert(output !=null)
   }
 }
